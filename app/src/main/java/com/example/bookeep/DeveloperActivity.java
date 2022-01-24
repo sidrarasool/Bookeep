f60fc01723bb950b3f9ee2dea5b3ca4f1f4427fe
mahnoor510 <mahnoorsajid04@gmail.com>
2022-01-24 22:22:14 +0500

    new class pakages

--- app/src/main/java/com/example/bookeep/DeveloperActivity.java
+++ app/src/main/java/com/example/bookeep/DeveloperActivity.java
@@ -1,4 +1,13 @@
-package com.example.bookeep;
+package com.example.android.bookeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
 
 public class DeveloperActivity {
 }
