aaf407f125db473a207625d2238804c5e1bf6ab1
mahnoor510 <mahnoorsajid04@gmail.com>
2022-01-24 19:32:30 +0500

    new class

---
+++ app/src/main/java/com/example/bookeep/ViewDevelopersFragment.java
@@ -0,0 +1,27 @@
+package com.example.bookeep;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ViewDevelopersFragment {
+    
+}
