f4a91e0e9058f6b26b40811e508593efb873443f
mahnoor510 <mahnoorsajid04@gmail.com>
2022-01-24 23:44:45 +0500

    DeveloperActivity class

--- app/src/main/java/com/example/bookeep/DeveloperActivity.java
+++ app/src/main/java/com/example/bookeep/DeveloperActivity.java
@@ -9,5 +9,55 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class DeveloperActivity {
+public class DeveloperActivity extends AppCompatActivity {
+    private static final int NUM_PAGES = 2;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+
+        viewPager = findViewById(R.id.vp_dev);
+
+        pagerAdapter = new ScreeSlidePagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    private class ScreeSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreeSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return AddDevFragment.newInstance();
+                case 1:
+                    return ViewDevelopersFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        public CharSequence getPageTitle(int position){
+            switch (position){
+                case 0:
+                    return "Add Dev";
+                case 1:
+                    return "View Devs";
+                default:
+                    return null;
+            }
+        }
+    }
 }
